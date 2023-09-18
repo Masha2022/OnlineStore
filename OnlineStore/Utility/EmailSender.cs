0ae49b37ec18191f6b085cf5c1adeef2e57d3e92
Masha <107428174+Masha2022@users.noreply.github.com>
2023-09-18 17:00:59 +0300

    AddEmailSender

---
+++ OnlineStore/Utility/EmailSender.cs
@@ -0,0 +1,40 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+public class EmailSender:IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        return Execute(email, subject, htmlMessage);
+    }
+
+    public async Task<Response> Execute(string email, string subject, string body)
+    {
+        
+        // Извлечение API-ключа из переменных окружения
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+    
+        // Проверка на null
+        if (apiKey == null) 
+        {
+            throw new ArgumentNullException("API Key not found in environment variables.");
+        }
+
+        // Инициализация клиента SendGrid
+        var client = new SendGridClient(apiKey);
+
+        // Создание нового сообщения
+        var from = new EmailAddress("margatuk@proton.me", "OnlineStore");
+        var to = new EmailAddress(email, "Example User");
+        var plainTextContent = "This is your plain text content"; // или генерируйте из body
+        var htmlContent = body;
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+        // Отправка сообщения и возврат ответа
+        return await client.SendEmailAsync(msg);
+    }
+}
